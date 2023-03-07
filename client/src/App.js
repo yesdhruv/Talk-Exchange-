e98da95c6e2eb788886407b0a6616a21c630a715
Dhruv Rawat <89683914+yesdhruv@users.noreply.github.com>
2023-03-07 23:53:29 +0530

    added

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Replies from "./components/Replies";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={<Home />} />
+        <Route path='/:id/replies' element={<Replies />} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
